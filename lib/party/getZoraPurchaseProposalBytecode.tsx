bcb8975c91e2423ab0f16e50f47c661632cbb975
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-03 21:04:00 -0500

    I update for Kasbeel drop on Base Mainnet.

--- lib/party/getZoraPurchaseProposalBytecode.tsx
+++ lib/party/getZoraPurchaseProposalBytecode.tsx
@@ -2,7 +2,6 @@ import {
   Address,
   encodeAbiParameters,
   encodeFunctionData,
-  encodePacked,
   parseAbiParameters,
   parseEther,
 } from 'viem';
@@ -10,17 +9,17 @@ import { zoraCreator1155ImplABI } from '@zoralabs/protocol-deployments';
 import { AbiCoder } from 'ethers';
 
 const getZoraPurchaseProposalBytecode = () => {
-  const collectionAddress = '0x78aef8d0bdba272f34d86bf6b8ba96aa54584932' as Address;
+  const collectionAddress = '0xab00c2fbfb2cb909f64ba5808ed8c5078e8f1a32' as Address;
   const price = 0n;
   const zoraFee = parseEther('0.000777');
   const value = price + zoraFee;
-  const minter = '0xd34872BE0cdb6b09d45FCa067B07f04a1A9aE1aE' as Address;
-  const tokenId = 2n;
+  const minter = '0x04E2516A2c207E84a1839755675dfd8eF6302F0a' as Address;
+  const tokenId = 1n;
   const quantity = 1n;
-  const recipient = '0xb5acded340d66678f01097818940a0f028dafb8d' as Address;
+  const recipient = '0x4273e913cb67fcc350065d850f942352b74c18a8' as Address;
   const minterArguments = encodeAbiParameters(parseAbiParameters('address x, string y'), [
     recipient,
-    'HELLO WORLD',
+    'Based in Colombia 🇨🇴',
   ]);
   const mintReferral = recipient;
   const data = encodeFunctionData({
