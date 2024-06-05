8c4234cb13da0dab5ea6cd0de9f0256abc8709fd
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-04 20:59:54 -0500

    I init <InputGroup />

--- components/LandingPage/LandingPageContent.tsx
+++ components/LandingPage/LandingPageContent.tsx
@@ -3,37 +3,15 @@ import GenerateButton from '../GenerateButton';
 import MadeBySweets from '../MadeBySweets';
 import Output from '../Output';
 import LandingPageHeader from './LandingPageHeader';
-import Input from '../Input';
+import InputGroup from '../InputGroup';
 
 const LandingPageContent = () => {
-  const {
-    proposalData,
-    fundsRecipient,
-    setFundsRecipient,
-    saleStrategy,
-    setSaleStrategy,
-    collectionAddress,
-    setCollectionAddress,
-  } = useProvider();
+  const { proposalData } = useProvider();
 
   return (
     <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center sm:gap-8 md:px-6">
       <LandingPageHeader />
-      <Input
-        placeholder="collectionAddress"
-        value={collectionAddress}
-        onChange={(e: any) => setCollectionAddress(e.target.value)}
-      />
-      <Input
-        placeholder="minter"
-        value={saleStrategy}
-        onChange={(e: any) => setSaleStrategy(e.target.value)}
-      />
-      <Input
-        placeholder="recipient"
-        value={fundsRecipient}
-        onChange={(e: any) => setFundsRecipient(e.target.value)}
-      />
+      <InputGroup />
       {proposalData && (
         <div>
           <div>proposalData</div>
