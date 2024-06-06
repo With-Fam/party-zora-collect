aff04db431dcb16b72157bd564e3e2b23a709ab5
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-05 19:31:28 -0500

    I add link to Based in Colombia 🇨 Party.

--- components/MadeBySweets/MadeBySweets.tsx
+++ components/MadeBySweets/MadeBySweets.tsx
@@ -1,8 +1,14 @@
 import { GITHUB_REPO } from '@/lib/consts';
 
 const MadeBySweets = () => (
-  <div>
-    Based in Colombia 🇨🇴
+  <div className="flex flex-col gap-3">
+    <a
+      href="https://base.party.app/party/0x4273e913cb67fcc350065d850f942352b74c18a8"
+      target="_blank"
+      className="flex justify-center items-center gap-5"
+    >
+      Based in Colombia 🇨🇴
+    </a>
     <a href={GITHUB_REPO} target="_blank" className="flex justify-center items-center gap-5">
       made with <img className="rounded rounded-full" src="/images/fam-logo.jpeg" width="50" /> by
       fam
